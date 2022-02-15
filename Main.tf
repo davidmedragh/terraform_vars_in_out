dcdeeaa185d647febd546bea4289f716d79f7d24
DAVID MEDRAGH <david.medragh@gmail.com>
2022-02-15 19:12:55 +0100

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -1,7 +1,8 @@
 provider "aws" {
     region = "${var.AWS_REGION}"
-    access_key = "AKIA32GFGEOFCZ57B4N3"
-    secret_key = "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"
+    access_key = ""
+    secret_key = ""
+"
 }
 
 resource "aws_instance" "my_ec2_instance" {
